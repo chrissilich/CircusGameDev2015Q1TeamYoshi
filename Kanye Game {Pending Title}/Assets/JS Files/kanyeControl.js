5357e2a29444134e0b14f28a638256fb6bb88170
GoddessC <goddesselixir@gmail.com>
2015-02-22 23:12:38 -0500

    Revert "Revert 73951b6..de64b23"
    This reverts commit 10836743b454e864121d24fa4bc42a6dca249b2f.

--- Kanye Game {Pending Title}/Assets/JS Files/kanyeControl.js
+++ Kanye Game {Pending Title}/Assets/JS Files/kanyeControl.js
@@ -1,4 +1,67 @@
-﻿#pragma strict
+﻿// #pragma strict
+
+// public var jumpSpeed:float = 8;
+// public var acceleration:float = 0.2;
+// public var maxSpeed:float = 15;
+// public var slideSpeed: float = 25;
+
+// function Start () {
+// 	Debug.Log("Character Start Function");
+	
+// 	// rigidbody2D.velocity.y = 8;
+// }
+
+// function FixedUpdate () {
+	
+// 	var sprite:Transform = this.transform.Find("Sprite"); //ref to sprite object. game anim obj
+// 	var animController: Animator = sprite.GetComponent("Animator");	
+// 	// Debug.Log(animController);
+// 	animController.SetInteger("State",0);
+	
+		
+// 	if (rigidbody2D.velocity.x < maxSpeed) {
+// 		rigidbody2D.velocity.x += acceleration;
+// 	}
+	
+// 	var start = transform.position;
+// 	start.y -= 1.1;
+	
+// 	Debug.DrawRay( start, -Vector2.up * 0.1, Color.red, 1 );
+	
+// 	var grounded:RaycastHit2D = Physics2D.Raycast( start, -Vector2.up, 0.1 );
+
+// 	if(grounded.collider){
+// 		animController.SetInteger("State",0);
+// 	} else{
+// 		animController.SetInteger("State",1);
+// 	}
+
+// 	if (Input.GetKey(KeyCode.UpArrow) && grounded.collider) { //if he's on the ground n up key is hit
+// 		Debug.Log("JUMP");
+// 		rigidbody2D.velocity.y = jumpSpeed;
+// 	} else if(Input.GetKey(KeyCode.DownArrow) && grounded.collider){
+// 		Debug.Log('SLIDE');
+// 		// Debug.Log(slideSpeed);
+// 		rigidbody2D.velocity.x = slideSpeed;
+// 	} else{
+// 		rigidbody2D.velocity.x = maxSpeed;
+// 	}
+	
+	
+// 	if (transform.position.y < -2) {
+// 		rigidbody2D.velocity.x = 0;
+// 		transform.position.x = -68.66307;
+// 		transform.position.y = 2;
+		
+// 		Application.LoadLevel("Level 1"); //loads any level
+// 	}
+	
+// 	// Debug.Log(rigidbody2D.velocity.x);
+// }
+
+
+
+#pragma strict
 
 public var jumpSpeed:float = 8;
 public var acceleration:float = 0.2;
@@ -7,7 +70,6 @@ public var slideSpeed: float = 25;
 
 function Start () {
 	Debug.Log("Character Start Function");
-	
 	// rigidbody2D.velocity.y = 8;
 }
 
@@ -26,9 +88,9 @@ function FixedUpdate () {
 	var start = transform.position;
 	start.y -= 1.1;
 	
-	Debug.DrawRay( start, -Vector3.up * 0.1, Color.red, 1 );
+	Debug.DrawRay( start, -Vector2.up * 0.1, Color.red, 1 );
 	
-	var grounded:RaycastHit = Physics.Raycast( start, -Vector3.up, 0.1 );
+	var grounded:RaycastHit2D = Physics2D.Raycast( start, -Vector2.up, 0.1 );
 
 	if(grounded.collider){
 		animController.SetInteger("State",0);
@@ -40,18 +102,16 @@ function FixedUpdate () {
 		Debug.Log("JUMP");
 		rigidbody2D.velocity.y = jumpSpeed;
 	} else if(Input.GetKey(KeyCode.DownArrow) && grounded.collider){
-		var acceleration:float = -5;
 		Debug.Log('SLIDE');
 		// Debug.Log(slideSpeed);
-		rigidbody.velocity.x = slideSpeed;
-		rigidbody.velocity.y = acceleration;
+		rigidbody2D.velocity.x = slideSpeed;
 	} else{
-		rigidbody.velocity.x = maxSpeed;
+		rigidbody2D.velocity.x = maxSpeed;
 	}
 	
 	
 	if (transform.position.y < -2) {
-		rigidbody.velocity.x = 0;
+		rigidbody2D.velocity.x = 0;
 		transform.position.x = -68.66307;
 		transform.position.y = 2;
 		
